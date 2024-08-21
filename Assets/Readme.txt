f862996fb6f4f7ee313a8bfb251cfb3858110a42
Keijiro Takahashi <keijiro@gmail.com>
2024-08-21 23:11:16 +0900

    Reimplementing with new Metavido and URP

--- Assets/Readme.txt
+++
@@ -1,2 +0,0 @@
-The sample assets are contained in the Packages directory. Please navigate to
-Packages > BibcamVFX Samples > Runtime to reveal them.
---
+++ Packages/jp.keijiro.metavido.vfxgraph/Scripts/VFXMetavidoBinder.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using UnityEngine.VFX;
+using UnityEngine.VFX.Utility;
+using Metavido.Decoder;
+
+namespace Metavido {
+
+[AddComponentMenu("VFX/Property Binders/Metavido Binder")]
+[VFXBinder("Metavido")]
+public sealed class VFXMetavidoBinder : VFXBinderBase
+{
+    [SerializeField] MetadataDecoder _decoder = null;
+    [SerializeField] TextureDemuxer _demux = null;
+
+    public string ColorMapProperty
+      { get => (string)_colorMapProperty;
+        set => _colorMapProperty = value; }
+
+    public string DepthMapProperty
+      { get => (string)_depthMapProperty;
+        set => _depthMapProperty = value; }
+
+    public string RayParamsProperty
+      { get => (string)_rayParamsProperty;
+        set => _rayParamsProperty = value; }
+
+    public string InverseViewProperty
+      { get => (string)_inverseViewProperty;
+        set => _inverseViewProperty = value; }
+
+    public string DepthRangeProperty
+      { get => (string)_depthRangeProperty;
+        set => _depthRangeProperty = value; }
+
+    [VFXPropertyBinding("UnityEngine.Texture2D"), SerializeField]
+    ExposedProperty _colorMapProperty = "ColorMap";
+
+    [VFXPropertyBinding("UnityEngine.Texture2D"), SerializeField]
+    ExposedProperty _depthMapProperty = "DepthMap";
+
+    [VFXPropertyBinding("UnityEngine.Vector4"), SerializeField]
+    ExposedProperty _rayParamsProperty = "RayParams";
+
+    [VFXPropertyBinding("UnityEngine.Matrix4x4"), SerializeField]
+    ExposedProperty _inverseViewProperty = "InverseView";
+
+    [VFXPropertyBinding("UnityEngine.Vector2"), SerializeField]
+    ExposedProperty _depthRangeProperty = "DepthRange";
+
+    public override bool IsValid(VisualEffect component)
+      => _decoder != null && _demux != null &&
+         component.HasTexture(_colorMapProperty) &&
+         component.HasTexture(_depthMapProperty) &&
+         component.HasVector4(_rayParamsProperty) &&
+         component.HasMatrix4x4(_inverseViewProperty) &&
+         component.HasVector2(_depthRangeProperty);
+
+    public override void UpdateBinding(VisualEffect component)
+    {
+        // Do nothing if metadata is not ready.
+        var meta = _decoder.Metadata;
+        if (!meta.IsValid) return;
+
+        // Camera parameters
+        var ray = RenderUtils.RayParams(meta);
+        var iview = RenderUtils.InverseView(meta);
+
+        // Property update
+        component.SetTexture(_colorMapProperty, _demux.ColorTexture);
+        component.SetTexture(_depthMapProperty, _demux.DepthTexture);
+        component.SetVector4(_rayParamsProperty, ray);
+        component.SetMatrix4x4(_inverseViewProperty, iview);
+        component.SetVector2(_depthRangeProperty, meta.DepthRange);
+    }
+
+    public override string ToString()
+      => $"Metavido : {_colorMapProperty}, {_depthMapProperty}";
+}
+
+} // namespace Metavido

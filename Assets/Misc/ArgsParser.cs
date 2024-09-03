7ad85d649a0d2de9d928a0a38dc8db965ef12fc8
Keijiro Takahashi <keijiro@gmail.com>
2024-09-03 17:43:41 +0900

    Add "lite" version

--- Assets/Misc/ArgsParser.cs
+++ Assets/Misc/ArgsParser.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.Video;
+using UnityEngine.Rendering.Universal;
 
 public sealed class ArgsParser : MonoBehaviour
 {
@@ -7,7 +8,11 @@ public sealed class ArgsParser : MonoBehaviour
     {
         var args = System.Environment.GetCommandLineArgs();
         for (var i = 1; i < args.Length; i++)
+        {
+            if (args[i] == "--lite")
+                Camera.main.GetComponent<UniversalAdditionalCameraData>().SetRenderer(1);
             if (i < args.Length - 1 && args[i] == "--sourceURL")
                 GetComponent<VideoPlayer>().url = args[++i];
+        }
     }
 }

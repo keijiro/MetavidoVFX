eae2e096fbe0d31cd288f7d69340e00a5e108a31
Keijiro Takahashi <keijiro@gmail.com>
2024-09-05 17:43:53 +0900

    Lite mode

--- Assets/Misc/ArgsParser.cs
+++ Assets/Misc/ArgsParser.cs
@@ -4,13 +4,21 @@ using UnityEngine.Rendering.Universal;
 
 public sealed class ArgsParser : MonoBehaviour
 {
+    [SerializeField] GameObject[] _optionalVfxList = null;
+
+    void ApplyLiteSettings()
+    {
+        Camera.main.GetComponent<UniversalAdditionalCameraData>().SetRenderer(1);
+        foreach (var go in _optionalVfxList) go.SetActive(false);
+    }
+
     void Start()
     {
         var args = System.Environment.GetCommandLineArgs();
         for (var i = 1; i < args.Length; i++)
         {
             if (args[i] == "--lite")
-                Camera.main.GetComponent<UniversalAdditionalCameraData>().SetRenderer(1);
+                ApplyLiteSettings();
             if (i < args.Length - 1 && args[i] == "--sourceURL")
                 GetComponent<VideoPlayer>().url = args[++i];
         }

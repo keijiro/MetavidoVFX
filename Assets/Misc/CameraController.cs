e3b7f519396905dbffa2ccb01b2876e23b26af27
Keijiro Takahashi <keijiro@gmail.com>
2025-04-10 15:28:54 +0900

    improved camera controller

--- Assets/Misc/CameraController.cs
+++ Assets/Misc/CameraController.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UIElements;
 using Unity.Mathematics;
+using Klak.Math;
 
 public sealed class CameraController : MonoBehaviour
 {
@@ -9,17 +10,31 @@ public sealed class CameraController : MonoBehaviour
     [SerializeField] UIDocument _ui = null;
     [SerializeField] Camera _camera = null;
     [SerializeField] Transform _pivotNode = null;
-    [SerializeField] Transform _distanceNode = null;
+    [SerializeField] Transform _slideNode = null;
 
     #endregion
 
     #region Camera controlling parameters
 
-    [field:SerializeField] public float AngleSpeed = 90;
-    [field:SerializeField] public float PitchLimit = 60;
-    [field:SerializeField] public float DistanceSpeed = 4;
-    [field:SerializeField] public float2 DistanceLimit = math.float2(3, 6);
-    [field:SerializeField] public float2 FovRange = math.float2(20, 45);
+    [Space]
+    [field:SerializeField] public float RotationSpeed = 2;
+    [field:SerializeField] public float PitchLimit = 1;
+    [Space]
+    [field:SerializeField] public float ZoomSpeed = 0.5f;
+    [field:SerializeField] public float2 DistanceRange = math.float2(3, 6);
+    [Space]
+    [field:SerializeField] public float2 FovRange = math.float2(20, 30);
+    [Space]
+    [field:SerializeField] public float TweenSpeed = 5;
+    [field:SerializeField] public float DelayToReset = 3;
+
+    #endregion
+
+    #region Private members
+
+    float2 _rotation;
+    (float target, float current) _zoom;
+    float _idleTime;
 
     #endregion
 
@@ -27,19 +42,24 @@ public sealed class CameraController : MonoBehaviour
 
     void OnDragging(float2 delta)
     {
-        var r = (float3)_pivotNode.localEulerAngles;
-        r.x = (r.x + 180) % 360 - 180; // (0, 360) => (-180, 180)
-        r.xy += delta.yx * AngleSpeed;
-        r.x = math.clamp(r.x, -PitchLimit, PitchLimit);
-        _pivotNode.localEulerAngles = r;
+        // Rotation
+        _rotation += delta.yx * RotationSpeed;
+        _rotation.x = math.clamp(_rotation.x, -PitchLimit, PitchLimit);
+
+        // Pulling up to the minimum zoom value
+        _zoom.target = math.max(_zoom.target, 0.3334f);
+
+        // Not idle
+        _idleTime = 0;
     }
 
     void OnScrolling(float delta)
     {
-        var dist = _distanceNode.localPosition.z;
-        dist += DistanceSpeed * delta;
-        dist = math.clamp(dist, -DistanceLimit.y, -DistanceLimit.x);
-        _distanceNode.localPosition = new float3(0, 0, dist);
+        // Zoom
+        _zoom.target = math.saturate(_zoom.target - ZoomSpeed * delta);
+
+        // Not idle
+        _idleTime = 0;
     }
 
     #endregion
@@ -58,9 +78,34 @@ public sealed class CameraController : MonoBehaviour
 
     void Update()
     {
-        var dist = -_distanceNode.localPosition.z;
-        var ndist = (dist - DistanceLimit.x) / (DistanceLimit.y - DistanceLimit.x);
-        _camera.fieldOfView = math.lerp(FovRange.x, FovRange.y, ndist);
+        var dt = Time.deltaTime;
+
+        // Zoom-in by timeout
+        if ((_idleTime += dt) > DelayToReset)
+            _zoom.target = math.saturate(_zoom.target - dt);
+
+        // Smooth zoom
+        _zoom.current = ExpTween.Step(_zoom.current, _zoom.target, TweenSpeed);
+
+        // Rotation reset on full zoom-in
+        if (_zoom.current < 0.05f) _rotation = 0;
+
+        // Rotation calculation
+        var rot = quaternion.Euler(math.float3(_rotation, 0));
+        var rot_amp = math.saturate(_zoom.current * 3);
+        rot = math.slerp(quaternion.identity, rot, rot_amp);
+        rot = ExpTween.Step(_pivotNode.localRotation, rot, TweenSpeed);
+
+        // Distance calculation
+        var dist = math.lerp(DistanceRange.x, DistanceRange.y, _zoom.current);
+
+        // Field of view calculation
+        var fov = math.lerp(FovRange.x, FovRange.y, _zoom.current);;
+
+        // Scene object update
+        _pivotNode.localRotation = rot;
+        _slideNode.localPosition = math.float3(0, 0, -dist);
+        _camera.fieldOfView = fov;
     }
 
     #endregion
